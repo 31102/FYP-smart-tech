608732dbe250916c4d219c556093f43041843cfe
Awais Akram <mystzex@gmail.com>
2023-05-13 13:06:33 +0500

    first commit

---
+++ backend/config/database.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+
+
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify:false
+    })
+    .then((data) => {
+      console.log(`Mongodb connected with server: ${data.connection.host}`);
+    })
+    .catch((err) =>{
+      console.log(err)
+    });
+};
+
+module.exports = connectDatabase;
---
+++ frontend/src/component/layout/Header/Header.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { ReactNavbar } from "overlay-navbar"
+import logo from "../../../images/logo.png";
+
+const options = {
+    burgerColor: "#eb4034",
+    burgerColorHover: "#eb4034",
+    logo,
+    logoWidth: "20vmax",
+    navColor1: "white",
+    logoHoverSize: "10px",
+    logoHoverColor: "#eb4034",
+    link1Text: "Home",
+    link2Text: "Products",
+    link3Text: "Contact",
+    link4Text: "About",
+    link1Url: "/",
+    link2Url: "/products",
+    link3Url: "/contact",
+    link4Url: "/about",
+    link1Size: "1.3vmax",
+    link1Color: "rgba(35, 35, 35,0.8)",
+    nav1justifyContent: "flex-end",
+    nav2justifyContent: "flex-end",
+    nav3justifyContent: "flex-start",
+    nav4justifyContent: "flex-start",
+    link1ColorHover: "#eb4034",
+    link1Margin: "1vmax",
+    profileIconUrl: "/login",
+    profileIconColor: "rgba(35, 35, 35,0.8)",
+    searchIconColor: "rgba(35, 35, 35,0.8)",
+    cartIconColor: "rgba(35, 35, 35,0.8)",
+    profileIconColorHover: "#eb4034",
+    searchIconColorHover: "#eb4034",
+    cartIconColorHover: "#eb4034",
+    cartIconMargin: "1vmax",
+    logoAnimationTime:"0.5",
+    link1AnimationTime:"0.5",
+    link2AnimationTime:"0.5",
+    link3AnimationTime:"0.5",
+    link4AnimationTime:"0.5",
+    searchIconAnimationTime:"0.5",
+    cartIconAnimationTime:"0.5",
+    profileIconAnimationTime:"0.5",
+};
+
+function Header() {
+    return <ReactNavbar {...options} />
+}
+
+export default Header
